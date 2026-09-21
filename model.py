6d72e32f427c02fe818383cc1a3d491b5576c918
Joep van Opdorp <joep@homepc.homepc.local>
2026-09-21 17:40:55 +0200

    Making model bigger

--- model.py
+++ model.py
@@ -276,8 +276,8 @@ class RWKVLanguageModel(nn.Module):
 
 MODEL_CONFIG = dict(
     vocab_size=50257,
-    dim=572,
-    n_layers=11,
+    dim=768,
+    n_layers=12,
     heads=12,
     rank=32,
     ffn_mult=4,
